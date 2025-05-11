3244b3c347869aed4f330bc8868646031d831d7e
JHAYCO9 <juanmunozrojas95@gmail.com>
2025-05-10 20:06:15 -0500

    ,

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, jsonify
-from SCRAP import scrape_matches
+from SCRAP import scrape_matches  # Asegúrate de que el archivo se llame exactamente SCRAP.py (mayúsculas)
+
 import os
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({"message": "API de scrapping funcionando. Usa /scrape para obtener datos."})
 
 @app.route('/scrape')
 def scrape():
@@ -10,5 +15,5 @@ def scrape():
     return jsonify(data)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))  # Puerto definido por Railway
     app.run(host="0.0.0.0", port=port)
